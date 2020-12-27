0942fd50305d145f4dcd0369cda12cd44a7a6574
Peter St. John <peterc.stjohn@gmail.com>
2020-12-27 07:47:01 -0700

    updating task layer

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # fairseq-uniparc
+
+## Install and setup
+
+First, create a conda environment with 
+`conda env create -f fairseq.yml`
+
+Then, install my fork of fairseq from [pstjohn/fairseq](https://github.com/pstjohn/fairseq) with `pip install --editable ./` in the resulting conda environment. 
+
+On eagle, my environment is kept in `/projects/deepgreen/pstjohn/envs/fairseq`. Use `--prefix=...` during `conda env create` to specify a custom directory.
--- go_annotation/fairseq_layers/task.py
+++ go_annotation/fairseq_layers/task.py
@@ -1,8 +1,24 @@
-from fairseq.tasks import register_task, SentencePredictionTask
+import logging
+import os
+
+import numpy as np
+from fairseq.data import (
+    ConcatSentencesDataset, IdDataset,
+    NestedDictionaryDataset, NumSamplesDataset, NumelDataset,
+    OffsetTokensDataset,
+    PrependTokenDataset,
+    RawLabelDataset, RightPadDataset,
+    RollDataset, SortDataset, StripTokenDataset, data_utils)
+from fairseq.data.shorten_dataset import maybe_shorten_dataset
+from fairseq.tasks import register_task
+from fairseq.tasks.sentence_prediction import SentencePredictionTask
+
+logger = logging.getLogger(__name__)
+
 
 @register_task("sentence_labeling")
 class SentenceLabelingTask(SentencePredictionTask):
-    
+
     @classmethod
     def setup_task(cls, args, **kwargs):
         assert args.num_classes > 0, "Must set --num-classes"
@@ -15,15 +31,108 @@ class SentenceLabelingTask(SentencePredictionTask):
         )
         logger.info("[input] dictionary: {} types".format(len(data_dict)))
 
-        label_dict = None
-        if not args.regression_target:
-            # load label dictionary
-            label_dict = cls.load_dictionary(
-                args,
-                os.path.join(args.data, "label", "dict.txt"),
-                source=False,
-            )
-            logger.info("[label] dictionary: {} types".format(len(label_dict)))
-        else:
-            label_dict = data_dict
+        label_dict = data_dict
         return cls(args, data_dict, label_dict)
+
+    def load_dataset(self, split, combine=False, **kwargs):
+        """Load a given dataset split (e.g., train, valid, test)."""
+
+        def get_path(key, split):
+            return os.path.join(self.args.data, key, split)
+
+        def make_dataset(key, dictionary):
+            split_path = get_path(key, split)
+
+            dataset = data_utils.load_indexed_dataset(
+                split_path,
+                dictionary,
+                self.args.dataset_impl,
+                combine=combine,
+            )
+            return dataset
+
+        input0 = make_dataset("input0", self.source_dictionary)
+        assert input0 is not None, "could not find dataset: {}".format(
+            get_path("input0", split)
+        )
+        input1 = make_dataset("input1", self.source_dictionary)
+
+        if self.args.init_token is not None:
+            input0 = PrependTokenDataset(input0, self.args.init_token)
+
+        if input1 is None:
+            src_tokens = input0
+        else:
+            if self.args.separator_token is not None:
+                input1 = PrependTokenDataset(input1, self.args.separator_token)
+
+            src_tokens = ConcatSentencesDataset(input0, input1)
+
+        with data_utils.numpy_seed(self.args.seed):
+            shuffle = np.random.permutation(len(src_tokens))
+
+        src_tokens = maybe_shorten_dataset(
+            src_tokens,
+            split,
+            self.args.shorten_data_split_list,
+            self.args.shorten_method,
+            self.args.max_positions,
+            self.args.seed,
+        )
+
+        dataset = {
+            "id": IdDataset(),
+            "net_input": {
+                "src_tokens": RightPadDataset(
+                    src_tokens,
+                    pad_idx=self.source_dictionary.pad(),
+                ),
+                "src_lengths": NumelDataset(src_tokens, reduce=False),
+            },
+            "nsentences": NumSamplesDataset(),
+            "ntokens": NumelDataset(src_tokens, reduce=True),
+        }
+
+        if self.args.add_prev_output_tokens:
+            prev_tokens_dataset = RightPadDataset(
+                RollDataset(src_tokens, 1),
+                pad_idx=self.dictionary.pad(),
+            )
+            dataset["net_input"].update(
+                prev_output_tokens=prev_tokens_dataset,
+            )
+
+        label_path = "{0}.label".format(get_path("label", split))
+        if os.path.exists(label_path):
+            def parse_regression_target(i, line):
+                values = line.split()
+                return [int(x) for x in values]
+
+            with open(label_path) as h:
+                dataset.update(
+                    target=RawLabelDataset(
+                        [
+                            parse_regression_target(i, line.strip())
+                            for i, line in enumerate(h.readlines())
+                        ]
+                    )
+                )
+
+        nested_dataset = NestedDictionaryDataset(
+            dataset,
+            sizes=[src_tokens.sizes],
+        )
+
+        if self.args.no_shuffle:
+            dataset = nested_dataset
+        else:
+            dataset = SortDataset(
+                nested_dataset,
+                # shuffle
+                sort_order=[shuffle],
+            )
+
+        logger.info("Loaded {0} with #samples: {1}".format(split, len(dataset)))
+
+        self.datasets[split] = dataset
+        return self.datasets[split]
